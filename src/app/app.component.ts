0c85695d456cc43ba0d35643a8c0d51336c5c319
CatAvadani <catalinaavadani@gmail.com>
2024-09-21 05:39:51 +0200

    User component displays avatar and name dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { DUMMY_USERS } from './user/dummy-users';
 import { UserComponent } from './user/user.component';
 
 @Component({
@@ -9,4 +10,6 @@ import { UserComponent } from './user/user.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+}
