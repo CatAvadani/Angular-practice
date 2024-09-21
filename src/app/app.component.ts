e73e56dce45baa17fa8ec93deb8db944a5902fb6
CatAvadani <catalinaavadani@gmail.com>
2024-09-21 17:11:10 +0200

    Update tasks component and create separate files for the interfaces. Display tasks depending on the user.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from './dummy-users';
 import { HeaderComponent } from './header/header.component';
 import { TasksComponent } from './tasks/tasks.component';
-import { DUMMY_USERS } from './user/dummy-users';
 import { UserComponent } from './user/user.component';
 
 @Component({
